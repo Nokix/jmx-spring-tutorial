c3acaf118dff4fe712887582c42ae31456578891
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-03-02 10:41:18 +0100

    Logger hinzugefügt

--- src/main/java/conteco/gmbh/jmxspringtutorial/MyController.java
+++ src/main/java/conteco/gmbh/jmxspringtutorial/MyController.java
@@ -2,6 +2,8 @@ package conteco.gmbh.jmxspringtutorial;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedOperationParameter;
@@ -11,8 +13,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ManagedResource(objectName = "MyMBeans:category=MBean,objectName=testBean")
+@ManagedResource(objectName = "MyMBeans:category=MBean,objectName=testBean", log = true)
 public class MyController {
+    private  final Log logger = LogFactory.getLog(MyController.class);
+
     private final Counter counter;
 
     public MyController(MeterRegistry meterRegistry) {
@@ -21,12 +25,14 @@ public class MyController {
 
     @ManagedAttribute
     public double getCounter() {
+        logger.info("get the counter");
         return counter.count();
     }
 
     @GetMapping("/greeting")
     @ManagedOperation
     public String greeting() {
+        logger.info("greet");
         counter.increment();
         return "Hallo";
     }
@@ -35,6 +41,7 @@ public class MyController {
     @ManagedOperation
     @ManagedOperationParameter(name = "name", description = "Name for greeting")
     public String greetWithName(@PathVariable String name) {
+        logger.info("greet "+ name);
         counter.increment();
         return "Hallo " + name;
     }
